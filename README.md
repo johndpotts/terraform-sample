e61ab3bd16fc38b230204801539bb0fc3bc59b2d
John Potts <jdpotts189@gmail.com>
2021-08-03 22:17:58 -0400

    added lambda and iam

---
+++ README.md
@@ -0,0 +1,2 @@
+# Terraform Sample
+This is a quick sample app I've created in both terraform and cloudformation in order to get a little more experience with cloudformation
--- lambda.tf
+++ lambda.tf
@@ -1,3 +1,19 @@
-resource "aws_lambda_function" "get_widget" {
+resource "aws_lambda_function" "tf_lambda" {
+  filename      = "./lambdas/lambda.zip"
+  function_name = "tf-lambda"
+  role          = aws_iam_role.tf_lambda_role.arn
+  handler       = "index.handler"
 
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+  source_code_hash = filebase64sha256("./lambdas/lambda.zip")
+
+  runtime = "nodejs12.x"
+
+  environment {
+    variables = {
+      env = "test"
+    }
+  }
 }
